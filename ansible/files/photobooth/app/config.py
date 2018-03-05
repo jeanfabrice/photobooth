c925634dd8060476afd68a91c26e02b15b8ccb01
Jean-Fabrice Bobo <jeanfabrice.bobo@orange.com>
2018-03-06 00:56:42 +0100

    update

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -9,4 +9,4 @@ montagedir = "/mnt/photobooth"
 
 def render(source, dest):
   cadres = ['cadre.png', 'cadre1.png', 'cadre2.png', 'cadre3.png']
-  subprocess.check_call(['convert',source,'-resize','1350x900','-gravity','center','-rotate','-2.4','-extent','1800x1200',os.path.join(os.path.dirname(os.path.realpath(__file__)),random.choice(cadres)),'-composite',dest])
+  subprocess.check_call(['convert',source,'-resize','1300x867','-gravity','center','-rotate','-2.3','-extent','1748x1181ı',os.path.join(os.path.dirname(os.path.realpath(__file__)),random.choice(cadres)),'-composite', '-extent', '111%' ,dest])
