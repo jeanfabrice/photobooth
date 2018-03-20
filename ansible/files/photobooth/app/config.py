9b08e459c92bed605044c8318f1760abc9dfd384
Jean-Fabrice <jeanfabrice@mbp.bobo-rousselin.com>
2018-03-20 17:20:05 +0100

    update

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -2,11 +2,21 @@ import subprocess
 import os
 import random
 
-frames     = 1
-counter    = 2
-imagedir   = "capture.d"
-montagedir = "/mnt/photobooth"
+
+frames      = 1
+counter     = 2
+imagedir    = "capture.d"
+montagedir  = "/mnt/photobooth"
+withleds    = True
+pressbutton = True
+knock       = True
+knockserver = ['projector']
 
 def render(source, dest):
   cadres = ['cadre.png', 'cadre1.png', 'cadre2.png', 'cadre3.png']
-  subprocess.check_call(['convert',source,'-resize','1300x867','-gravity','center','-rotate','-2.3','-extent','1748x1181ı',os.path.join(os.path.dirname(os.path.realpath(__file__)),random.choice(cadres)),'-composite', '-extent', '111%' ,dest])
+  printpath = os.path.join(os.path.dirname(os.path.splitext(dest)[0]),'print')
+  printdest = os.path.join(printpath, os.path.basename(dest))
+  if not os.path.exists(printpath):
+    os.makedirs(printpath)
+  subprocess.check_call(['convert', source, '-resize', '1300x867', '-gravity', 'center', '-rotate', '-2.3', '-extent', '1748x1181', os.path.join(os.path.dirname(os.path.realpath(__file__)), random.choice(cadres)), '-composite', dest])
+  subprocess.check_call(['convert', dest, '-extent', '111%', printdest])
--- ansible/files/photobooth/app/photobooth.py
+++ ansible/files/photobooth/app/photobooth.py
@@ -10,12 +10,48 @@ import gphoto2 as gp
 import time
 import uuid
 import time
-from config import render
 import config
 import signal
 from functools import partial
-import fcntl
 import socket
+import RPi.GPIO as GPIO
+
+tensleds      = [27, 4, 17, 22]
+unitsleds     = [11, 10, 9, 5]
+frameleds     = [19, 6, 13,26]
+buttonled     = 14
+buttonswitch  = 18
+
+def displayNumOn7Segments(num, segment):
+    binnum = "{0:04b}".format(num)
+    for idx, val in enumerate(binnum):
+        GPIO.output(segment[idx], GPIO.LOW if val=='0' else GPIO.HIGH)
+
+def countdown(start):
+    for c in xange(start, 0, -1):
+        if config.withleds:
+            displayNumOn7Segments(int(c/10), tensleds)
+            displayNumOn7Segments(c%10, unitsleds)
+        else:
+            print(c)
+        time.sleep(1)
+    if config.withleds:
+        displayNumOn7Segments(0, unitsleds)
+    else:
+        print(0)
+
+def switchLed(channel, state='on'):
+    GPIO.output(channel, GPIO.HIGH if state=='on' else GPIO.LOW)
+
+def initGPIO():
+    if config.pressbutton or config.withleds:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        for channel in tensleds+unitsleds+frameleds+[buttonled]:
+            GPIO.setup(channel, GPIO.OUT)
+        for channel in [buttonswitch]:
+            GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    blank7Segments()
 
 def knock(host, knock_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,10 +63,21 @@ def knock(host, knock_port):
 def handle_signal(camera, signum, frame):
     print ("\nCleaning camera link and exit")
     gp.check_result(gp.gp_camera_exit(camera))
+    GPIO.cleanup()
     sys.exit(0)
 
-def displayLed(c, f):
-    print('counter: {0} - Left frame: {1}'.format(c, f))
+def wairForButtonPressed():
+    while True:
+        input_state = GPIO.input(buttonswitch)
+        if input_state == False:
+            switchLed(buttonled, 'off')
+            break
+
+def blank7Segments():
+    if config.withleds:
+        displayNumOn7Segments(10, tensleds)
+        displayNumOn7Segments(10, unitsleds)
+        displayNumOn7Segments(10, frameleds)
 
 def configurecamera(camera):
     setup = {"output": "PC", "viewfinder": 1}
@@ -39,26 +86,10 @@ def configurecamera(camera):
         setup['capturetarget'] = "card"
     
     for c, v in setup.items():
-        print('setup "%s" camera property with value: %s'%(c,v))
+        print('Setting up "%s" camera property with value: %s'%(c,v))
         try:
             widget = gp.check_result(gp.gp_camera_get_single_config(camera,c))
-        except gp.GPhoto2Error:
-            print ('I/O error. Please restart camera')
-            gp.check_result(gp.gp_camera_exit(camera))
-            main()
-        except:
-            raise
-
-        try:
             gp.check_result(gp.gp_widget_set_value(widget,v))
-        except gp.GPhoto2Error:
-            print ('I/O error. Please restart camera')
-            gp.check_result(gp.gp_camera_exit(camera))
-            main()
-        except:
-            raise
-
-        try:
             gp.check_result(gp.gp_camera_set_single_config(camera,c,widget))
         except gp.GPhoto2Error:
             print ('I/O error. Please restart camera')
@@ -67,15 +98,16 @@ def configurecamera(camera):
         except:
             raise
 
-def captureSerie(camera,now):
+def captureFrame(camera,now):
     sequence     = []
     currentFrame = 0
     
     while currentFrame < config.frames:
-        for c in xrange(config.counter,0,-1):
-            displayLed(c,config.frames-currentFrame)
-            time.sleep(1)
-        
+        if config.withleds:
+            displayNumOn7Segments(config.frames-currentFrame, frameleds)
+        else:
+            print('Left frame: {0}'.format(config.frames-currentFrame))
+        countdown(config.counter)
         while True:
             try:
                 file_path = gp.check_result(gp.gp_camera_capture(camera, gp.GP_CAPTURE_IMAGE))
@@ -114,10 +146,11 @@ def captureSerie(camera,now):
     return capturedirectory
 
 
-
 def main():
     if not os.path.exists(config.montagedir):
         os.makedirs(config.montagedir)
+
+    initGPIO()
 
     logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=logging.ERROR)
 
@@ -128,54 +161,68 @@ def main():
     except:
         raise
 
+    #create camera object
     camera = gp.check_result(gp.gp_camera_new())
 
+    # Handle SIGTERM and SIGINT to properly reset and exit camera before dying
     signal.signal(signal.SIGTERM, partial(handle_signal,camera))
     signal.signal(signal.SIGINT, partial(handle_signal,camera))
     
-    # Wait for camera to connect before continue
+    # Wait for camera to connect before continuing
     while True:
         try:
             gp.check_result(gp.gp_camera_init(camera))
         except gp.GPhoto2Error as ex:
             if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
-                print ('no camera, retry in 2 seconds')
+                print ('camera not detected, retrying in 2 seconds')
                 time.sleep(2)
                 continue
             if ex.code == gp.GP_ERROR_IO:
-                print ('I/O error. Restart camera, will retry in 5 seconds')
+                print ('I/O error. Restart camera, retrying in 5 seconds')
                 time.sleep(5)
                 continue
-            if ex.code == gp.GP_ERROR_BAD_PARAMETERS:
-                break
+            #if ex.code == gp.GP_ERROR_BAD_PARAMETERS:
+            #    break
+            blank7Segments()
+            raise
+        except:
+            blank7Segments()
             raise
         break
 
-    print('Camera connected!')
+    print('Camera connected')
  
     try:
         configurecamera(camera)
     except:
+        blank7Segments()
         raise
 
     #Capture frame endlessly
     while True:
-        try:
-            time.sleep(10)
-            #input("press button to continue")
-        except SyntaxError:
-            pass
-        
+        if config.withleds:
+            displayNumOn7Segments(int(config.counter/10,tensleds))
+            displayNumOn7Segments(config.counter%10,unitsleds)
+            displayNumOn7Segments(config.frames,frameleds)
+
+        if config.pressbutton:
+            switchLed(buttonled)
+            wairForButtonPressed()
+
+        #define current time to timestamp frame name
         now = time.strftime("%Y%m%d_%Hh%Mm%Ss")
         
-        capturedirectory = captureSerie(camera,now)
+        capturedirectory = captureFrame(camera,now)
         
         try:
+            #Call rendering method from config module with source file list and destination file path
             config.render(os.path.join(capturedirectory,'*.jpg'), os.path.join(config.montagedir,'%s.jpg'%now))
         except:
             raise
         
-        knock('projector', 10000)
+        if config.knock:
+            for knockserver in config.knockservers:
+                knock(knockserver, 10000)
 
 
 if __name__ == "__main__":
