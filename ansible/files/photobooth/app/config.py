4ce301435fc1efe7645565f7b855cf4ec4457ed4
jeanfabrice <github@bobo-rousselin.com>
2018-04-08 22:02:53 +0200

    debug config

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -5,7 +5,7 @@ import random
 
 frames       = 1
 counter      = 10
-imagedir     = "capture.d"
+imagedir     = "/mnt/photobooth/capture.d"
 montagedir   = "/mnt/photobooth"
 withleds     = True
 pressbutton  = True
@@ -18,4 +18,4 @@ def render(source, dest):
   if not os.path.exists(printpath):
     os.makedirs(printpath)
   subprocess.check_call(['convert', source, '-resize', '1300x867', '-gravity', 'center', '-rotate', '-2.3', '-extent', '1748x1181', os.path.join(os.path.dirname(os.path.realpath(__file__)), random.choice(cadres)), '-composite', dest])
-  subprocess.check_call(['convert', dest, '-extent', '111%', '-gravity', 'center', os.path.join(printpath, os.path.basename(dest))])
+  subprocess.check_call(['convert', dest, '-gravity', 'center', '-extent', '111%', os.path.join(printpath, os.path.basename(dest))])
