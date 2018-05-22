428eba1de28ad165d9a22b2c3fe5dd18ef075ca6
jeanfabrice <github@bobo-rousselin.com>
2018-05-22 08:20:16 +0200

    strip exif data before printing

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -17,4 +17,4 @@ def render(source, dest):
   if not os.path.exists(printpath):
     os.makedirs(printpath)
   subprocess.check_call(['convert', source, '-resize', '1392x928', '-gravity', 'center', '-extent', '1748x1181+0+40', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cadre.png'), '-composite', dest])
-  subprocess.check_call(['convert', dest, '-gravity', 'center', '-extent', '111%', os.path.join(printpath, os.path.basename(dest))])
+  subprocess.check_call(['convert', dest, '-strip', '-gravity', 'center', '-extent', '111%', os.path.join(printpath, os.path.basename(dest))])
