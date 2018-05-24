1c28ea494f10fcf91e65f4826b4d22d8be8cd442
jeanfabrice <github@bobo-rousselin.com>
2018-05-24 09:37:33 +0200

    Nouveau cadre, plus eclatant

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -16,7 +16,7 @@ def render(source, dest):
   printpath = os.path.join(os.path.dirname(os.path.splitext(dest)[0]),'print')
   if not os.path.exists(printpath):
     os.makedirs(printpath)
-  subprocess.check_call(['convert', source, '-strip', '-resize', '1350x900', '-gravity', 'center', '-extent', '1800x1200', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cadre-rouge.png'), '-composite', dest])
+  subprocess.check_call(['convert', source, '-strip', '-resize', '1200x819', '-gravity', 'center', '-extent', '1800x1200', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cadre.png'), '-composite', dest])
   subprocess.check_call(['convert', dest, '-strip', '-gravity', 'center', '-extent', '111%', os.path.join(printpath, os.path.basename(dest))])
   subprocess.check_call(['lp', '-o', 'raw', dest])
 
