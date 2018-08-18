ca0367f7be09549855d3149acd10fbebca13dfa4
jeanfabrice <github@bobo-rousselin.com>
2018-08-18 10:49:01 +0200

    random choices of cadres

--- ansible/files/photobooth/app/config.py
+++ ansible/files/photobooth/app/config.py
@@ -13,8 +13,9 @@ knock        = True
 knockservers = ['projector']
 
 def render(source, dest):
+  cadres = ['cadre1.png', 'cadre2.png', 'cadre3.png', 'cadre4.png']
   printpath = os.path.join(os.path.dirname(os.path.splitext(dest)[0]),'print')
   if not os.path.exists(printpath):
     os.makedirs(printpath)
-  subprocess.check_call(['convert', source, '-strip', '-resize', '1200x819', '-gravity', 'center', '-extent', '1800x1200', os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cadre.png'), '-composite', dest])
+  subprocess.check_call(['convert', source, '-strip', '-resize', '1200x819', '-gravity', 'center', '-extent', '1800x1200', os.path.join(os.path.dirname(os.path.realpath(__file__)), random.choice(cadres)), '-composite', dest])
 
